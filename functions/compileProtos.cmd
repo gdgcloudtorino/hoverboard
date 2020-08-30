57da1242e13270a2e8172c29aa0921c870fe744a
cfrancescop <c.francesco.p@gmail.com>
2020-08-30 17:55:49 +0200

    aggiunto moment

--- functions/compileProtos.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\node_modules\google-gax\build\tools\compileProtos.js" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\node_modules\google-gax\build\tools\compileProtos.js" %*
-)
