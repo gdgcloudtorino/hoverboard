7fcc16ad1d2898d4e82cca7656da5ebfe9a1b5a8
Gregorio Palamà <gregoriopalama@gmail.com>
2020-10-16 18:20:45 +0200

    pulsanti allineati per i giochi e cartella lotto

--- src/pages/game-page.ts
+++ src/pages/game-page.ts
@@ -6,7 +6,12 @@ import '../elements/footer-block';
 import '../elements/polymer-helmet';
 import '../elements/hoverboard-icons';
 import '../elements/shared-styles';
-class GamePage extends PolymerElement {
+import { dialogsActions, lottoActions, toastActions } from '../redux/actions';
+import { DIALOGS } from '../redux/constants';
+import { store } from '../redux/store';
+import { ReduxMixin } from '../mixins/redux-mixin';
+
+class GamePage extends ReduxMixin(PolymerElement) {
   static get template() {
     return html`
       <style>
@@ -40,48 +45,30 @@ class GamePage extends PolymerElement {
         <div class="container" layout  justified>
           <h1 class="centered" style="ont-size: 2.1em;"></h1>
         </div>
-        <div class="container" style="padding-top: 0;" layout horizontal justified>
-          <paper-button primary class="cta-button animated icon-right centered" on-tap="_addCardRequest">
+        <div class="container" style="padding-top: 0;text-align: center;" layout horizontal centered>
+          <paper-button primary class="cta-button animated icon-right centered">
             <span>I giochi</span>
+          </paper-button>
+          <paper-button primary class="cta-button animated icon-right centered" on-tap="_addCardRequest">
+            <span>Richiedi una cartella</span>
+          </paper-button>
+          <paper-button primary class="cta-button animated icon-right centered">
+            <span>Iscriviti ai giochi</span>
+          </paper-button>
+          <paper-button primary class="cta-button animated icon-right centered">
+            <span>Leaderboard</span>
           </paper-button>
         </div>
       </div>
 
-      <div>
-      <div class="container" layout  justified>
-        <h1 class="centered" style="ont-size: 2.1em;"></h1>
-      </div>
-      <div class="container" style="padding-top: 0;" layout horizontal justified>
-        <paper-button primary class="cta-button animated icon-right centered" on-tap="_addCardRequest">
-          <span>Richiedi una cartella</span>
-        </paper-button>
-      </div>
-    </div>
-
-    <div>
-    <div class="container" layout  justified>
-      <h1 class="centered" style="ont-size: 2.1em;"></h1>
-    </div>
-    <div class="container" style="padding-top: 0;" layout horizontal justified>
-      <paper-button primary class="cta-button animated icon-right centered" on-tap="_addCardRequest">
-        <span>Iscriviti ai giochi</span>
-      </paper-button>
-    </div>
-    </div>
-
-    <div>
-    <div class="container" layout  justified>
-      <h1 class="centered" style="ont-size: 2.1em;"></h1>
-    </div>
-    <div class="container" style="padding-top: 0;" layout horizontal justified>
-      <paper-button primary class="cta-button animated icon-right centered" on-tap="_addCardRequest">
-        <span>Leaderboard</span>
-      </paper-button>
-    </div>
-    </div>
     <footer-block></footer-block>
     `;
   }
+
+  private active = false;
+  private user = {};
+  private cardRequestAdding = false;
+  private cardRequestAddingError = {};
 
   static get is() {
     return 'game-page';
@@ -94,7 +81,50 @@ class GamePage extends PolymerElement {
         type: String,
         value: 'GAME',
       },
+      user: Object,
+      cardRequestAdding: {
+        type: Boolean,
+        observer: '_cardRequestAddingChanged',
+      },
+      cardRequestAddingError: Object,
     };
+  }
+
+  stateChanged(state: import('../redux/store').State) {
+    super.stateChanged(state);
+    this.setProperties({
+      user: state.user,
+      cardRequestAdding: state.lotto.adding,
+      cardRequestAddingError: state.lotto.addingError,
+    });
+  }
+
+  _addCardRequest() {
+    if (!(this.user as any).signedIn) {
+      toastActions.showToast({ message: '{$ lottoBlock.mustBeSigned $}' });
+      return;
+    }
+    dialogsActions.openDialog(DIALOGS.SUBSCRIBE, {
+      title: '{$ lottoBlock.form.title $}',
+      submitLabel: '{$ lottoBlock.form.submitLabel $}',
+      nameFieldLabel: '{$ lottoBlock.form.name $}',
+      lastNameFieldLabel: '{$ lottoBlock.form.lastName $}',
+      emailFieldValue: (this.user as any).email,
+      submit: (data) => {
+        store.dispatch(lottoActions.addCardRequest(data));
+      },
+    });
+  }
+
+  _cardRequestAddingChanged(newCardRequestAdding, oldCardRequestAdding) {
+    if (oldCardRequestAdding && !newCardRequestAdding) {
+      if (this.cardRequestAddingError) {
+        store.dispatch(dialogsActions.setDialogError(DIALOGS.SUBSCRIBE));
+      } else {
+        dialogsActions.closeDialog(DIALOGS.SUBSCRIBE);
+        toastActions.showToast({ message: '{$ lottoBlock.toast $}' });
+      }
+    }
   }
 }
 
